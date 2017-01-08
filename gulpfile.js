27ee0fd25cbdb3646321b944e47552d7cbbc2456
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2017-01-08 10:49:26 +0100

    add gulp tasks for building docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp')
 var shell = require('gulp-shell')
 
-gulp.task('build-docs', shell.task('make html', {cwd: './rstdocs'}))
+gulp.task('clean-docs', shell.task('make clean && rm -rf doxygenbuild/html/* && rm -rf ../docs/* && mkdir ../docs/classmap', {cwd: './rstdocs'}))
 
-gulp.task('clean-docs', shell.task('make clean', {cwd: './rstdocs'}))
+gulp.task('build-docs', shell.task('make html && cp -r build/html/* ../docs/', {cwd: './rstdocs'}))
+gulp.task('build-classmap', shell.task('doxygen doxygen.conf && cp -r doxygenbuild/html/* docs/classmap/', {cwd: './'}))
 
 gulp.task('docs', ['build-docs'], function() {
   gulp.watch(
